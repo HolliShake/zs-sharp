a7ba3594064ddaf0747240f173e82c4297ea8359
HolliShake <redondophilippandrewroa.dev@gmail.com>
2026-06-02 07:24:10 +0800

    feat: add push, pop, and clear methods to Array class; enhance method handling

--- Array.cs
+++ Array.cs
@@ -2,7 +2,7 @@ namespace zscript;
 
 public class Array : IBuiltin
 {
-    private static readonly string[] Methods = ["length"];
+    private static readonly string[] Methods = ["push", "length"];
     
     public static bool HasMethod(string methodName)
     {
@@ -13,9 +13,34 @@ public class Array : IBuiltin
     {
         return methodName switch
         {
+            "push" => ArrayPushMethod,
+            "pop" => ArrayPopMethod,
+            "clear" => ArrayClearMethod,
             "length" => ArrayLengthMethod,
             _ => throw new NotImplementedException($"method {methodName} not implemented")
         };
+    }
+
+    private static ZsValue ArrayPushMethod(Vm vm, ZsValue[] args)
+    {
+        if (args.Length < 2) return ZsValue.FromErrorMessage(vm.ErrorClass, "push() expects at least 1 argument", vm.BuildTracebackFromFrame());
+        args[0].Array().AddRange(args.Skip(1).Reverse());
+        return vm.NullSingleton;
+    }
+
+    private static ZsValue ArrayPopMethod(Vm vm, ZsValue[] args)
+    {
+        var arr = args[0].Array();
+        if (arr.Count == 0) return ZsValue.FromErrorMessage(vm.ErrorClass, "pop() called on empty array", vm.BuildTracebackFromFrame());
+        var item = arr[^1];
+        arr.RemoveAt(arr.Count - 1);
+        return item;
+    }
+
+    private static ZsValue ArrayClearMethod(Vm vm, ZsValue[] args)
+    {
+        args[0].Array().Clear();
+        return vm.NullSingleton;
     }
 
     private static ZsValue ArrayLengthMethod(Vm vm, ZsValue[] args)
